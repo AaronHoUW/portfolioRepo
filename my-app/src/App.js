c7e2df1a347d8d22a02e97d035ebddb06a8d820c
aaronho@uw.edu <aaronho@aaronh.local>
2023-11-14 18:48:02 -0800

    Added Banner Image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,9 @@ function App() {
 return (
     <>
         <Navbar />
+        <div className="banner">
+            <img className="bannerImage" src="/img/marygates.jpg"></img>
+        </div>
         <div>
             <p>Testing</p>
             <img src="https://remywiki.com/images/thumb/1/17/ROCK_THE_PARTY.png/400px-ROCK_THE_PARTY.png" alt="Rock The Party Jacket" className="drs_ima1" />
