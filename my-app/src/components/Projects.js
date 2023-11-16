8ebbc6ad00ec9ec25b1a0d0343ad43b868477adf
aaronho@uw.edu <aaronho@aaronh.local>
2023-11-15 21:36:10 -0800

    Add User Feedback on hover

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -30,7 +30,7 @@ export function ProjectPage(props) {
                         <ProjectHeader>{props.title}</ProjectHeader>
                         <p>Lorem ipsum dolor sit amet consectetur. Sed sem malesuada sed egestas nibh vitae purus. Dolor pharetra placerat vivamus faucibus nulla odio convallis aliquet urna. Suspendisse quis ridiculus et convallis purus. Sollicitudin sit etiam non montes diam sagittis mauris in.</p>
                     </div>
-                    <ProjectButtonLink href={data.link} target="_blank" rel="noopener noreferrer"><ProjectButton>View Project</ProjectButton></ProjectButtonLink>
+                    <ProjectButtonLink href={data.link} target="_blank" rel="noopener noreferrer"><ProjectButton className="website_button">View Project</ProjectButton></ProjectButtonLink>
                 </ProjectText>
                 <img src="https://remywiki.com/images/thumb/1/17/ROCK_THE_PARTY.png/400px-ROCK_THE_PARTY.png" alt="Rock The Party Jacket" className="drs_ima1" />
             </ProjectInfo>
