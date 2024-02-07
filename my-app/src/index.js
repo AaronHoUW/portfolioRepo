7fd2f01124a1acff607fabeee1a9c3fbf85e647b
aaronho@uw.edu <aaronho@aaronh.local>
2024-02-07 14:34:23 -0800

    Adjust files for publish

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID
+}
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
