0147f31ed9f818975ca0743b88bc3f6462dbe58a
aaronho@uw.edu <aaronho@aaronh.local>
2024-03-06 13:35:39 -0800

    Added navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,18 +1,40 @@
-import React from "react";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import React, {useState} from "react";
 import {
     NavBarContainer,
+    NavBarItems,
     HashLinks,
-    NavBarHeader
+    HashLinksContainer,
+    NavBarHeader,
+    DropDownMenu,
+    MenuList,
+    MenuItems
 } from './Navbar.tsx'
 
 function Navbar() {
+    const [isClicked, setIsClicked] = useState()
+
+    const onClickMenu = () => {
+        setIsClicked(!isClicked)
+    }
+
     return (
         <NavBarContainer>
-            <NavBarHeader>Aaron Ho</NavBarHeader>
-            <div style={{'paddingTop': '10px'}}>
-                <HashLinks to="/#Projects" smooth>Projects</HashLinks>
-                <HashLinks to="/#Experience" smooth>Experience</HashLinks>
-            </div>
+            <NavBarItems>
+                <NavBarHeader>Aaron Ho</NavBarHeader>
+                <HashLinksContainer>
+                    <HashLinks to="/#Projects" smooth>Projects</HashLinks>
+                    <HashLinks to="/#Experience" smooth>Experience</HashLinks>
+                </HashLinksContainer>
+                <DropDownMenu onClick={onClickMenu}>
+                    <IconContext.Provider value={{ size: "50px" }}><AiOutlineCaretDown className={"navbar_dropdown_" + isClicked}/></IconContext.Provider>
+                </DropDownMenu>
+            </NavBarItems>
+            <MenuList className={"navbar_menu_" + isClicked}>
+                <MenuItems><HashLinks to="/#Projects" smooth>Projects</HashLinks></MenuItems>
+                <MenuItems><HashLinks to="/#Experience" smooth>Experience</HashLinks></MenuItems>
+            </MenuList>
         </NavBarContainer>
     )
 }
--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { HashLink } from 'react-router-hash-link';
+import { device } from "./device";
 
 export const NavBarContainer = styled.div`
     z-index: 1;
@@ -12,17 +13,31 @@ export const NavBarContainer = styled.div`
     background-color: rgba(234, 234, 234, 0.5);
     overflow: hidden;
     position: fixed;
+    flex-direction: column;
 `;
+
+export const NavBarItems = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
 
 export const NavBarHeader = styled.h1`
     display: flex;
     flex-direction: row;
-    // font-family: 'Libre Bodoni';
     font-weight: bold;
     font-size: 40px;
     margin: 0px;
     margin-left: 2rem;
 `;
+
+export const HashLinksContainer = styled.div`
+    padding-top: 10px;
+    display: none;
+
+    @media${device.tablet}{
+        display: block;
+    }
+`
 
 export const HashLinks = styled(HashLink)`
     text-decoration: none;
@@ -33,3 +48,29 @@ export const HashLinks = styled(HashLink)`
     color: black;
     margin-right: 2rem;
 `;
+
+export const DropDownMenu = styled.a`
+    display: block;
+    margin-right: 1rem;
+
+    @media${device.tablet}{
+        display: none;
+    }
+`
+
+export const MenuList = styled.ul`
+    padding-inline-start: 30px;
+
+    @media${device.tablet}{
+        display: none;
+    }
+`
+
+export const MenuItems = styled.li`
+    display: block;
+    padding: 1rem 0rem;
+
+    @media${device.tablet}{
+        display: none;
+    }
+`
